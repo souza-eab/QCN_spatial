860c1accf0bd7bd5af6a12b9beb6a2d7e873a8e1
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-08-30 18:54:19 -0300

    Update 0_2_Rasterize.R

--- Steps/0_2_Rasterize.R
+++ Steps/0_2_Rasterize.R
@@ -38,30 +38,21 @@ for (i in 1:nrow(tiles)) {
   mask <- st_as_stars(mask)
   
   ## rasterize each field
-  r_cagb <- st_rasterize(vec_i['cagb'], template= mask)
-  r_cbgb <- st_rasterize(vec_i['cbgb'], template= mask)
-  r_cdw  <- st_rasterize(vec_i['cdw'], template= mask)
-  r_clitter <- st_rasterize(vec_i['clitter'], template= mask)
-  r_total <- st_rasterize(vec_i['ctotal4inv'], template= mask)
-  r_class <- st_rasterize(vec_i['C7_MAPBIOM'], template= mask)
-  
-  ## exportar
-  #write_stars(r_cagb, paste0('/output/cagb/', 'tile_', i, '_cagb.tif'), type="Float32", drive="GTiff")
-  #write_stars(r_cbgb, paste0('./output/cbgb/', 'tile_', i, '_cbgb.tif'), type="Float32", drive="GTiff")
-  #write_stars(r_cdw, paste0('./output/cdw/', 'tile_', i, '_cdw.tif'), type="Float32", drive="GTiff")
-  #write_stars(r_clitter, paste0('./output/clitter/', 'tile_', i, '_clitter.tif'), type="Float32", drive="GTiff")
-  #write_stars(r_cagb, paste0('./output/total/', 'tile_', i, '_total.tif'), type="Float32", drive="GTiff")
-  #write_stars(r_class, paste0('./output/', 'tile_', i, '_c7_qcnclass.tif'), type="Float32", drive="GTiff")
+  r_cagb <- st_rasterize(vec_i['c_agb'], template= mask)
+  r_cbgb <- st_rasterize(vec_i['c_bgb'], template= mask)
+  r_cdw  <- st_rasterize(vec_i['c_dw'], template= mask)
+  r_clitter <- st_rasterize(vec_i['c_litter'], template= mask)
+  r_total <- st_rasterize(vec_i['c_total4INV'], template= mask)
+  r_class <- st_rasterize(vec_i['MapBiomas_C8'], template= mask)
   
   
   output <-"C:/Users/edriano.souza/GitHub/2022_2_QCN_rectify_v2/Biomass_rectify_c8/Stp1_Rectify_QCN_and_C8-MapBiomas/Data_rectify_class/Raster/"
-  write_stars(r_cagb, paste0(output, 'tile_', i, '_cagb.tif'), type="Float32", drive="GTiff")
-  write_stars(r_cbgb, paste0(output, 'tile_', i, '_cbgb.tif'), type="Float32", drive="GTiff")
-  write_stars(r_cdw, paste0(output, 'tile_', i, '_cdw.tif'), type="Float32", drive="GTiff")
-  write_stars(r_clitter, paste0(output, 'tile_', i, '_clitter.tif'), type="Float32", drive="GTiff")
-  write_stars(r_total, paste0(output, 'tile_', i, '_total.tif'), type="Float32", drive="GTiff")
-  write_stars(r_class, paste0(output, 'tile_', i, '_c7_qcnclass.tif'), type="Float32", drive="GTiff")
-  
+  write_stars(r_cagb, paste0(output, 'tile_', i, '_c_agb.tif'), type="Float32", drive="GTiff")
+  write_stars(r_cbgb, paste0(output, 'tile_', i, '_c_bgb.tif'), type="Float32", drive="GTiff")
+  write_stars(r_cdw, paste0(output, 'tile_', i, '_c_dw.tif'), type="Float32", drive="GTiff")
+  write_stars(r_clitter, paste0(output, 'tile_', i, '_c_litter.tif'), type="Float32", drive="GTiff")
+  write_stars(r_total, paste0(output, 'tile_', i, '_c_total4INV.tif'), type="Float32", drive="GTiff")
+  write_stars(r_class, paste0(output, 'tile_', i, '_MapBiomas_C8.tif'), type="Float32", drive="GTiff")
   
   ## clean cache
   rm(tile_i, vec_i, mask, r_cagb, r_cbgb, r_cdw, r_clitter, r_total, r_class)
